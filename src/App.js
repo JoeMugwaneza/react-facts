c5a352dea1d34a8e6c1db152412fdf14dcd2abd8
Mugwaneza Joseph <muganezajoseph@gmail.com>
2022-08-27 18:21:27 +0200

    fixing darkmode and light

--- src/App.js
+++ src/App.js
@@ -4,10 +4,19 @@ import Main from "./components/Main"
 import './App.css';
 
 export default function App() {
+    const [darkMode, setDarkMode] = React.useState(true)
+
+
+    function toggleDarkMode(){
+      setDarkMode(prevMode => !prevMode)
+    }
     return (
         <div className="container">
-            <Navbar />
-            <Main />
+            <Navbar 
+                toggleDarkMode={toggleDarkMode}
+                darkMode={darkMode}
+            />
+            <Main darkMode={darkMode}/>
         </div>
     )
 }
